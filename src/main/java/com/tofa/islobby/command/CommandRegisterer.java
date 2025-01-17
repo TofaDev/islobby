eadf05ccc19745594824c8ab4d1a581245412ea1
TofaDev <tofadevelop@gmail.com>
2025-01-17 23:25:40 +0700

    migrate to new Lamp API

--- src/main/java/com/tofa/islobby/command/CommandRegisterer.java
+++ src/main/java/com/tofa/islobby/command/CommandRegisterer.java
@@ -2,7 +2,9 @@ package com.tofa.islobby.command;
 
 import com.tofa.islobby.IsLobby;
 import com.tofa.islobby.config.ConfigLoader;
-import revxrsal.commands.bukkit.BukkitCommandHandler;
+import revxrsal.commands.Lamp;
+import revxrsal.commands.bukkit.BukkitLamp;
+import revxrsal.commands.bukkit.actor.BukkitCommandActor;
 
 public class CommandRegisterer {
 
@@ -13,8 +15,10 @@ public class CommandRegisterer {
     }
 
     public void register() {
-        BukkitCommandHandler handler = BukkitCommandHandler.create(plugin);
-        handler.registerDependency(ConfigLoader.class, plugin.getConfigLoader());
-        handler.register(new SetLobbySpawnCommand());
+        Lamp<BukkitCommandActor> lamp = BukkitLamp.builder(plugin)
+                .dependency(ConfigLoader.class, plugin.getConfigLoader())
+                .build();
+
+        lamp.register(new SetLobbySpawnCommand());
     }
 }
